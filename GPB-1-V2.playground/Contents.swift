4318330df36fbd208497ffb3f9d6330d4eabd2b4
Egor Nenashev <nenashev.e.r@gmail.com>
2022-09-28 15:41:59 +0300

    Initial Commit

---
+++ GPB-1-V2.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+let n=200
+var stop: Int = 0
+var myArray: [Int] = []
+for _ in 0..<n{
+    myArray.append(Int.random(in:0..<1000))
+}
+print(myArray)
+for x in 0..<n-1{
+    if (stop != 0){
+        break
+    }
+    for m in x+1..<n{
+        if (stop != 0){
+            break
+        }
+        if myArray[x]==myArray[m]{
+            print(myArray[x])
+            print(" The first repeated element is ")
+            print(x)
+            print(" Element's index ")
+            stop = 1
+            break
+        }
+    }
+}
+if (stop==0){
+    print("-1")
+}
+
